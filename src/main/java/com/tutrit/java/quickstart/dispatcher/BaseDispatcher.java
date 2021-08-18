4a3357b117883ae47ff0ced39f69c779625fed70
Maksim Shelkovich <info@jrr.by>
2021-08-18 09:09:20 +0300

    FB-26: adds find all slots, unit and integration tests, mockito. Adds Base dispatcher to dispatch user comands

---
+++ src/main/java/com/tutrit/java/quickstart/dispatcher/BaseDispatcher.java
@@ -0,0 +1,31 @@
+package com.tutrit.java.quickstart.dispatcher;
+
+import com.tutrit.java.quickstart.bean.Slot;
+import com.tutrit.java.quickstart.controller.ScheduleCalendarController;
+import java.util.List;
+
+public class BaseDispatcher {
+    private final ScheduleCalendarController scheduleCalendarController;
+
+    public BaseDispatcher(final ScheduleCalendarController scheduleCalendarController) {
+        this.scheduleCalendarController = scheduleCalendarController;
+    }
+
+    public List<Slot> dispatch(String[] args) {
+        List<Slot> result = null;
+        switch (args[0]) {
+            case "/showSlots":
+                scheduleCalendarController.showAllSlots().forEach(System.out::println);
+                return scheduleCalendarController.showAllSlots();
+            case "/addSlot":
+                result = scheduleCalendarController.addSlot(args[1], args[2]);
+                result.forEach(System.out::println);
+                break;
+            case "/addBatchSlots":
+                result = scheduleCalendarController.addBatchSlots(args);
+                result.forEach(System.out::println);
+                break;
+        }
+        return null;
+    }
+}
